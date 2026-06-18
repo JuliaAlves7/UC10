599ec44e4b8f19fcf71390c3b2e2cc91956540a6
julia.aferreira7 <juliaalves.mrg@gmail.com>
2026-06-17 21:59:25 -0300

    atividade

---
+++ 02/ControleEstoque/ControleEstoque.API/Models/FormaPagamento.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+    public class FormaPagamento
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; } = true;
+        public ICollection<Pedido> Pedidos { get; set; } = new List<Pedido>();
+    }
+}
